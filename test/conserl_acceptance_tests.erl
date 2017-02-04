d52974c8f37e6abf19d39898419746456e51bc04
Carlo Cabanilla <carlo.cabanilla@gmail.com>
2017-02-03 18:05:38 -0800

    remove uuid dependency

--- test/conserl_acceptance_tests.erl
+++ test/conserl_acceptance_tests.erl
@@ -6,14 +6,14 @@
 
 simple_kv_put_get_test() ->
   application:ensure_all_started(conserl),
-  Key = binary_to_list(uuid:v4()),
-  Value = binary_to_list(uuid:v4()),
+  Key = binary_to_list(<<"foo">>),
+  Value = binary_to_list(<<"bar">>),
   conserl_kv:put(Key, Value),
   {ok, Response} = conserl_kv:get(Key),
   ?assertEqual(Value, maps:get(value, Response)).
 
 simple_kv_get_not_found_test() ->
-  Key = binary_to_list(uuid:v4()),
+  Key = binary_to_list(<<"baz">>),
   ?assertEqual({error, "Not Found"}, conserl_kv:get(Key)).
 
 
